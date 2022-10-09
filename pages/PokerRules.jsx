4bab9b54bfc3fde78d780140f708b12b21af4b62
Fernot94 <fernoo94@hotmail.com>
2022-10-09 15:04:22 +0100

    added rules assets

--- pages/PokerRules.jsx
+++ pages/PokerRules.jsx
@@ -3,74 +3,161 @@ import { Deck } from "../src/deck";
 import { getWinners } from "../src/drawsValidations";
 import { handToString } from "../src/rules";
 
-
 export default function PokerRules() {
-
-
   return (
     <div className="mainPokerRules">
       <img className="howToPlayImg" src="/how-to-play.jpg" alt="How to play" />
       <div className="learn">
         <h2>Learn how to play poker</h2>
-        <p>Whether you are new to the game of poker, or just need a refresher, PokerStars has what you need to learn how to play.</p>
+        <p>
+          Whether you are new to the game of poker, or just need a refresher,
+          PokerStars has what you need to learn how to play.
+        </p>
         <ul>
-          <li>Learn what beats what: We’ve got a handy guide to the hierarchy of poker hands, so you’ll always know when you’re holding the nuts!</li>
-          <li>We’ve got links to help you learn different types of poker game, plus free strategy advice.</li>
+          <li>
+            Learn what beats what: We’ve got a handy guide to the hierarchy of
+            poker hands, so you’ll always know when you’re holding the nuts!
+          </li>
+          <li>
+            We’ve got links to help you learn different types of poker game,
+            plus free strategy advice.
+          </li>
         </ul>
       </div>
       <div className="handRanking">
         <h2>Texas Hold’em - Hand Ranking</h2>
-        <p>Before you hit the online poker tables, you’ll need to familiarise yourself with the basic hand rankings and rules that govern Texas Hold’em. Here are the 10 hands every player should know before joining the action.</p>
+        <p>
+          Before you hit the online poker tables, you’ll need to familiarise
+          yourself with the basic hand rankings and rules that govern Texas
+          Hold’em. Here are the 10 hands every player should know before joining
+          the action.
+        </p>
         <div className="hands">
           <div>
-            <img className="handExemple" src="/royal-flush.png" alt="Royal Straight Flush" />
+            <img
+              className="handExemple"
+              src="/rules-assets/royal-flush.png"
+              alt="Royal Straight Flush"
+            />
             <h3>Royal Straight Flush</h3>
-            <p>Poker’s most famous hand, a royal straight flush, cannot be beaten. It consists of the ace, king, queen, jack and ten of a single suit.</p>
+            <p>
+              Poker’s most famous hand, a royal straight flush, cannot be
+              beaten. It consists of the ace, king, queen, jack and ten of a
+              single suit.
+            </p>
           </div>
           <div>
-            <img className="handExemple" src="/straight-flush.webp" alt="Straight Flush" />
+            <img
+              className="handExemple"
+              src="/rules-assets/straight-flush.webp"
+              alt="Straight Flush"
+            />
             <h3>Straight Flush</h3>
-            <p>Five cards in sequence, of the same suit. In the event of a tie, the highest rank at the top of the sequence wins.</p>
+            <p>
+              Five cards in sequence, of the same suit. In the event of a tie,
+              the highest rank at the top of the sequence wins.
+            </p>
           </div>
           <div>
-            <img className="handExemple" src="/four-of-a-kind.webp" alt="Four of a Kind" />
+            <img
+              className="handExemple"
+              src="/rules-assets/four-of-a-kind.webp"
+              alt="Four of a Kind"
+            />
             <h3>Four of a Kind</h3>
-            <p>Four cards of the same rank, and one side card or ‘kicker’. In the event of a tie, the player with the highest side card (‘kicker’) wins.</p>
+            <p>
+              Four cards of the same rank, and one side card or ‘kicker’. In the
+              event of a tie, the player with the highest side card (‘kicker’)
+              wins.
+            </p>
           </div>
           <div>
-            <img className="handExemple" src="/full-house.png" alt="Full House" />
+            <img
+              className="handExemple"
+              src="/rules-assets/full-house.png"
+              alt="Full House"
+            />
             <h3>Full House</h3>
-            <p>Three cards of the same rank, and two cards of a different, matching rank. In the event of a tie, the highest three matching cards wins.</p>
+            <p>
+              Three cards of the same rank, and two cards of a different,
+              matching rank. In the event of a tie, the highest three matching
+              cards wins.
+            </p>
           </div>
           <div>
-            <img className="handExemple" src="/flush.webp" alt="Flush" />
+            <img
+              className="handExemple"
+              src="/rules-assets/flush.webp"
+              alt="Flush"
+            />
             <h3>Flush</h3>
-            <p>Five cards of the same suit, not in sequence. In the event of a tie, the player holding the highest ranked card wins.</p>
+            <p>
+              Five cards of the same suit, not in sequence. In the event of a
+              tie, the player holding the highest ranked card wins.
+            </p>
           </div>
           <div>
-            <img className="handExemple" src="/Straight.webp" alt="Straight" />
+            <img
+              className="handExemple"
+              src="/rules-assets/Straight.webp"
+              alt="Straight"
+            />
             <h3>Straight</h3>
-            <p>Five non-suited cards in sequence. In the event of a tie, the highest ranking card at the top of the sequence wins.</p>
+            <p>
+              Five non-suited cards in sequence. In the event of a tie, the
+              highest ranking card at the top of the sequence wins.
+            </p>
           </div>
           <div>
-            <img className="handExemple" src="/three-of-a-kind.png" alt="Three of a Kind" />
+            <img
+              className="handExemple"
+              src="/rules-assets/three-of-a-kind.png"
+              alt="Three of a Kind"
+            />
             <h3>Three of a Kind</h3>
-            <p>Three cards of the same rank, and two unrelated side cards. In the event of a tie, the player with the highest, and if necessary, second-highest side card (‘kicker’) wins.</p>
+            <p>
+              Three cards of the same rank, and two unrelated side cards. In the
+              event of a tie, the player with the highest, and if necessary,
+              second-highest side card (‘kicker’) wins.
+            </p>
           </div>
           <div>
-            <img className="handExemple" src="/two-pair.webp" alt="Two Pair" />
+            <img
+              className="handExemple"
+              src="/rules-assets/two-pair.webp"
+              alt="Two Pair"
+            />
             <h3>Two Pair</h3>
-            <p>Two cards of matching rank, two cards of different matching rank, and one kicker. If both players have an identical Two Pair, the highest kicker wins.</p>
+            <p>
+              Two cards of matching rank, two cards of different matching rank,
+              and one kicker. If both players have an identical Two Pair, the
+              highest kicker wins.
+            </p>
           </div>
           <div>
-            <img className="handExemple" src="/pair.webp" alt="Pair" />
+            <img
+              className="handExemple"
+              src="/rules-assets/pair.webp"
+              alt="Pair"
+            />
             <h3>Pair</h3>
-            <p>Two cards of matching rank, and three unrelated side cards. In the event of a tie, the player with the highest, and if necessary, second or third-highest side card wins.</p>
+            <p>
+              Two cards of matching rank, and three unrelated side cards. In the
+              event of a tie, the player with the highest, and if necessary,
+              second or third-highest side card wins.
+            </p>
           </div>
           <div>
-            <img className="handExemple" src="/high-card.png" alt="High Card" />
+            <img
+              className="handExemple"
+              src="/rules-assets/high-card.png"
+              alt="High Card"
+            />
             <h3>High Card</h3>
-            <p>Any hand that does not qualify under the categories listed. In the event of a tie, the highest card wins, such as ‘ace-high’.</p>
+            <p>
+              Any hand that does not qualify under the categories listed. In the
+              event of a tie, the highest card wins, such as ‘ace-high’.
+            </p>
           </div>
         </div>
       </div>
