d703676fa796d54eacf44be6f4f6afea7179800b
Mauro Serrano <mauro.serrano@hotmail.com.br>
2022-10-10 14:50:54 +0100

    css home

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -7,13 +7,57 @@ export default function Home(props) {
   return (
     <div className="mainHome">
       <h2>Games</h2>
-      <img
-        src="/game-poker.webp"
-        alt="Poker Game"
-        className="gamePokerImg"
-        onClick={() => props.pageFunction("PokerMenu")}
-      />
-      <h3>Texas Hold'em</h3>
+      <div className="games">
+        <div className="poker">
+          <img
+            src="/games-assets/poker.jpg"
+            alt="Poker Game"
+            className="gamePokerImg"
+            onClick={() => props.pageFunction("PokerMenu")}
+          />
+          <h3>Texas Hold'em</h3>
+        </div>
+        <div className="roulette">
+          <img
+            src="/games-assets/roulette.jpg"
+            alt="Roulette"
+            className="disableGames"
+          />
+          <h3>Roulette</h3>
+        </div>
+        <div className="pool">
+          <img
+            src="/games-assets/8-ball-pool.png"
+            alt="8 Ball Poll"
+            className="disableGames"
+          />
+          <h3>8 Ball Poll</h3>
+        </div>
+        <div className="blackjack">
+          <img
+            src="/games-assets/blackjack.png"
+            alt="Blackjack"
+            className="disableGames"
+          />
+          <h3>Blackjack</h3>
+        </div>
+        <div className="ticTacToe">
+          <img
+            src="/games-assets/tic-tac-toe.jpg"
+            alt="Tic Tac Toe"
+            className="disableGames"
+          />
+          <h3>Tic Tac Toe</h3>
+        </div>
+        <div className="chess">
+          <img
+            src="/games-assets/chess.jpg"
+            alt="Chess"
+            className="disableGames"
+          />
+          <h3>Chess</h3>
+        </div>
+      </div>
     </div>
   );
 }
