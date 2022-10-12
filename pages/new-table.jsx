abd9dc279453491f3cd27625beac654ad44e0550
Fernot94 <fernoo94@hotmail.com>
2022-10-12 10:24:08 +0100

    added changes to globals.css

--- pages/new-table.jsx
+++ pages/new-table.jsx
@@ -7,33 +7,38 @@ export default function NewTable() {
 
   function setForm() {}
 
+  const onSelect = (e) => {
+    setMaxPlayers(e.target.value);
+    console.log(maxPlayers);
+  };
+
   return (
     <div className="createNewTable">
       <h2>Create new table</h2>
       <form>
         <span>Max players: </span>
-        <select>
-          <option value={setMaxPlayers(2)}>2</option>
-          <option value={setMaxPlayers(3)}>3</option>
-          <option value={setMaxPlayers(4)}>4</option>
-          <option value={setMaxPlayers(5)}>5</option>
-          <option value={setMaxPlayers(6)}>6</option>
-          <option value={setMaxPlayers(7)}>7</option>
-          <option value={setMaxPlayers(8)}>8</option>
-          <option value={setMaxPlayers(9)}>9</option>
+        <select onSelect={onSelect}>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+          <option value={6}>6</option>
+          <option value={7}>7</option>
+          <option value={8}>8</option>
+          <option value={9}>9</option>
         </select>
         <br />
         <span>Big Blind: </span>
-        <select>
-          <option value={setBigBlind(10)}>10</option>
-          <option value={setBigBlind(50)}>50</option>
-          <option value={setBigBlind(100)}>100</option>
-          <option value={setBigBlind(250)}>250</option>
-          <option value={setBigBlind(500)}>500</option>
-          <option value={setBigBlind(1000)}>1000</option>
-          <option value={setBigBlind(1500)}>1500</option>
-          <option value={setBigBlind(3000)}>3000</option>
-          <option value={setBigBlind(10000)}>10000</option>
+        <select onSelect={onSelect}>
+          <option value={10}>10</option>
+          <option value={50}>50</option>
+          <option value={100}>100</option>
+          <option value={250}>250</option>
+          <option value={500}>500</option>
+          <option value={1000}>1000</option>
+          <option value={1500}>1500</option>
+          <option value={3000}>3000</option>
+          <option value={10000}>10000</option>
         </select>
         <br />
         <span>In game chips: </span>
