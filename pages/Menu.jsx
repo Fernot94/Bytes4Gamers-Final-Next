8be7c2043efe5285d8c122807b5e488ba1e2ae34
Fernot94 <fernoo94@hotmail.com>
2022-10-12 12:06:50 +0100

    added some conditions to menu file

--- pages/Menu.jsx
+++ pages/Menu.jsx
@@ -2,28 +2,24 @@ import { useState } from "react";
 import { Deck } from "../src/deck";
 import { getWinners } from "../src/drawsValidations";
 import { handToString } from "../src/rules";
-import Link from 'next/link'
-import {useRouter} from 'next/router'
+import Link from "next/link";
+import { useRouter } from "next/router";
 
-export default function Menu() {
-  const router = useRouter()
+export default function Menu(props) {
+  const [usernameOrEmail, setUsernameOrEmail] = useState(props.user);
+  const router = useRouter();
 
   return (
-
     <div className="menu">
       <div className="abas">
         <Link href="/home">
           <a>
-            <button disabled={router.asPath === "/home"}>
-              Home
-            </button>
+            <button disabled={router.asPath === "/home"}>Home</button>
           </a>
         </Link>
         <Link href="/rules">
           <a>
-            <button disabled={router.asPath === "/rules"}>
-              Rules
-            </button>
+            <button disabled={router.asPath === "/rules"}>Rules</button>
           </a>
         </Link>
         <Link href="/how-to-play">
@@ -35,28 +31,26 @@ export default function Menu() {
         </Link>
         <Link href="/about-us">
           <a>
-            <button disabled={router.asPath === "/about-us"}>
-              About Us
-            </button>
+            <button disabled={router.asPath === "/about-us"}>About Us</button>
           </a>
         </Link>
       </div>
-      <div className="auth">
-        <Link href="/login">
-          <a>
-            <button disabled={false}>
-              Login
-            </button>
-          </a>
-        </Link>
-        <Link href="/register">
-          <a>
-            <button disabled={false}>
-              Register
-            </button>
-          </a>
-        </Link>
-      </div>
+      {usernameOrEmail === undefined ? (
+        <div className="auth">
+          <Link href="/login">
+            <a>
+              <button disabled={false}>Login</button>
+            </a>
+          </Link>
+          <Link href="/register">
+            <a>
+              <button disabled={false}>Register</button>
+            </a>
+          </Link>
+        </div>
+      ) : (
+        <div>ola</div>
+      )}
     </div>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
-import '../styles/globals.css'
-import Menu from "./Menu"
-import Foot from "./Foot"
+import "../styles/globals.css";
+import Menu from "./Menu";
+import Foot from "./Foot";
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <div>
-    <Menu/>
-    <Component {...pageProps} />
-    <Foot/>
+  return (
+    <div>
+      <Menu />
+      <Component {...pageProps} />
+      <Foot />
     </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
