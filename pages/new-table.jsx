bbf7b00eccb74caa09e3f8f1c310a3d93df666ba
Fernot94 <fernoo94@hotmail.com>
2022-10-14 12:06:41 +0100

    added stuff

--- pages/new-table.jsx
+++ pages/new-table.jsx
@@ -49,35 +49,37 @@ export default function NewTable() {
   return (
     <div className="createNewTable">
       <h2>Create new table</h2>
-      <form>
-        <label>Max players: </label>
-        <select id="maxPlayers" onChange={() => updateMaxPlayers()}>
-          <option value={2}>2</option>
-          <option value={3}>3</option>
-          <option value={4}>4</option>
-          <option value={5}>5</option>
-          <option value={6}>6</option>
-          <option value={7}>7</option>
-          <option value={8}>8</option>
-          <option value={9}>9</option>
-        </select>
+      <div className="createTableBody">
+        <form>
+          <label>Max players: </label>
+          <select id="maxPlayers" onChange={() => updateMaxPlayers()}>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
+            <option value={6}>6</option>
+            <option value={7}>7</option>
+            <option value={8}>8</option>
+            <option value={9}>9</option>
+          </select>
+          <br />
+          <label>Big Blind: </label>
+          <select id="bigBlind" onChange={() => updateBigBlind()}>
+            <option value={10}>10</option>
+            <option value={50}>50</option>
+            <option value={100}>100</option>
+            <option value={250}>250</option>
+            <option value={500}>500</option>
+            <option value={1000}>1000</option>
+            <option value={1500}>1500</option>
+            <option value={3000}>3000</option>
+            <option value={10000}>10000</option>
+          </select>
+          <br />
+        </form>
         <br />
-        <label>Big Blind: </label>
-        <select id="bigBlind" onChange={() => updateBigBlind()}>
-          <option value={10}>10</option>
-          <option value={50}>50</option>
-          <option value={100}>100</option>
-          <option value={250}>250</option>
-          <option value={500}>500</option>
-          <option value={1000}>1000</option>
-          <option value={1500}>1500</option>
-          <option value={3000}>3000</option>
-          <option value={10000}>10000</option>
-        </select>
-        <br />
-      </form>
-      <br />
-      <button onClick={() => setForm()}>Create</button>
+        <button onClick={() => setForm()}>Create</button>
+      </div>
       <Link href={"/poker-menu"}>
         <a>
           <button className="backButton">Go back</button>
